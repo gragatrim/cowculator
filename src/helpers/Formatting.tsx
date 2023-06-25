3334764448b6c8783afd2ba952e76ebf99771ce3
tristo7 <tristo7000@gmail.com>
2023-06-24 21:05:04 -0500

    bugfix: null handling for items lacking market

--- src/helpers/Formatting.tsx
+++ src/helpers/Formatting.tsx
@@ -1,4 +1,8 @@
-export function getFriendlyIntString(num: number, digits = 0): string {
+export function getFriendlyIntString(
+  num: number | undefined,
+  digits = 0
+): string {
+  if (!num) return "-1";
   return num.toLocaleString(undefined, {
     maximumFractionDigits: digits,
   });
